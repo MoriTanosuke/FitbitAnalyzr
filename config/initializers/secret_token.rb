97f66125f62093059be3c1984a47929035a36c89
Carsten Ringe <carsten.ringe@web.de>
2011-12-10 08:19:25 +0100

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fitbit::Application.config.secret_token = 'aea8a8b68625073534745ca600ad9b044fe061f4daf0c4c9b97988e272c3cdbb851245cbec3a788bdac19f805be16fdae44839b2689a394840db0d8cfb082e5c'
