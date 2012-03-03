1e22b8051f43753a13228f5eede79804221c4d00
Carsten Ringe <carsten.ringe@web.de>
2012-03-03 17:47:42 +0100

    Use decimal for sleep.user_id

--- db/migrate/20120302163201_add_user_to_sleeps.rb
+++ db/migrate/20120302163201_add_user_to_sleeps.rb
@@ -1,9 +1,9 @@
 class AddUserToSleeps < ActiveRecord::Migration
   def up
-    add_column :sleeps, :user_id, :number
+    add_column :sleeps, :user_id, :decimal
   end
 
   def down
-    remove_column :sleeps, :user_id, :number
+    remove_column :sleeps, :user_id, :decimal
   end
 end
