1456b3db0dda2c5357013096eb4459336e101b64
Carsten Ringe <carsten.ringe@web.de>
2012-03-03 17:57:50 +0100

    Change seed to valid values

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,4 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Emanuel', :city => cities.first)
-users = User.create({:name => 'test', :password => 'test'})
+users = User.create({:name => 'carsten@kopis.de', :password => 'online3d'})
