8d8bd2ad80d006fbd1d0b5ea36c508531ed6bfc8
Carsten Ringe <carsten.ringe@web.de>
2012-03-03 18:01:53 +0100

    Rename name to email in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,4 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Emanuel', :city => cities.first)
-users = User.create({:name => 'carsten@kopis.de', :password => 'online3d'})
+users = User.create({:email => 'test@localhost.org', :password => 'test'})
