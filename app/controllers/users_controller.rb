08452ed82e74b2b48ba0bbb180138941ce84a7b9
Carsten Ringe <carsten.ringe@web.de>
2012-03-06 19:43:26 +0100

    Reset session after destroying user.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -80,6 +80,7 @@ class UsersController < ApplicationController
   def destroy
     @user = User.find(params[:id])
     @user.destroy
+    session[:user_id] = nil
 
     respond_to do |format|
       flash[:success] = 'User removed.'
