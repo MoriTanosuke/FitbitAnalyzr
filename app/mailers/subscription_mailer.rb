6984a8f51ddebb71e282708a33741eb79abb2247
Carsten Ringe <carsten.ringe@web.de>
2012-03-12 18:39:34 +0100

    Fix notification mail. Add CC to welcome/goodbye mails.

--- app/mailers/subscription_mailer.rb
+++ app/mailers/subscription_mailer.rb
@@ -1,8 +1,8 @@
 class SubscriptionMailer < ActionMailer::Base
   default from: '"Carsten from Fitbit Analyzr" <carsten@kopis.de>'
 
-  def notification_received(user)
-    @user = user
+  def notification_received(notify)
+    @notification = notify
     mail(:to => 'carsten@kopis.de', :subject => 'Notification received')
   end
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,12 +4,12 @@ class UserMailer < ActionMailer::Base
   def welcome_email(user)
     @user = user
     @url = 'http://floating-sunrise-1622.heroku.com/login'
-    mail(:to => user.email, :subject => 'Welcome to Fitbit Analyzr')
+    mail(:to => user.email, :cc => 'carsten@kopis.de', :subject => 'Welcome to Fitbit Analyzr')
   end
 
   def goodbye_email(user)
     @user = user
     @feedback_url = ''
-    mail(:to => @user.email, :subject => 'Goodbye from Fitbit Analyzr')
+    mail(:to => @user.email, :cc => 'carsten@kopis.de', :subject => 'Goodbye from Fitbit Analyzr')
   end
 end
