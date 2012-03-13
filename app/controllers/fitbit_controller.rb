15a4bccac38bbe91ca281b56d968104d400392a7
Carsten Ringe <carsten.ringe@web.de>
2012-03-13 20:02:48 +0100

    Break up fitbit_controller and stub out API access.

--- app/controllers/fitbit_controller.rb
+++ app/controllers/fitbit_controller.rb
@@ -1,16 +1,24 @@
 class FitbitController < ApplicationController
 
+  def api_response(s, startDate, endDate)
+    if not current_user.fitbit.nil?
+      current_user.fitbit.client.get(['/1/user/-', s, 'date', startDate, endDate].join('/') + '.json').body
+    end
+  end
+
   def reload(series, date)
+    reload(series, date, 'today')
+  end
+
+  def reload(series, startDate, endDate)
     api = {}
     series.each do |s|
-      if not current_user.fitbit.nil?
-        begin
-          data = JSON.parse(current_user.fitbit.client.get('/1/user/-/' + s + '/date/' + date + '/today.json').body)
-          api = api.merge(data)
-        rescue SocketError
-          logger.error "Can not talk to fitbit"
-          nil
-        end
+      begin
+        data = JSON.parse(api_response(s, startDate, endDate))
+        api = api.merge(data)
+      rescue SocketError
+        logger.error "Can not talk to fitbit"
+        nil
       end
     end
     tapi = {}
---
+++ spec/controllers/sleeps_controller_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+describe SleepsController do
+  it "should update data for a given date" do
+    # stub out to avoid calls to fitbit
+    subject.stub(:api_response) {
+      '{"sleep/minutesAsleep":[{"dateTime":"1970-01-01","value":1},{"dateTime":"1970-01-02","value":2}]}'
+    }
+
+    api = subject.reload(['minutesAsleep'], '1970-01-01', '1970-01-02')
+    api['sleep/minutesAsleep'][0]['value'].should eq(1)
+  end
+end
+
